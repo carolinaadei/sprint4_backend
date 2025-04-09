6ad5c3e2fb64b3e5d7c22185cf4b46fdb233eb35
Henrique Collatto <chcollatto@gmail.com>
2025-04-09 15:30:45 -0300

    criacao pastas

---
+++ src/controller/user.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get, Req, Post} from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+  @Get()
+  findAll(@Req() request: Request): string {
+    console.log(`request.body: ${request.body}`); //teste 
+    return 'This action returns all users';
+  }
+
+  @Post()
+  create(): string {
+    return 'This action adds a new user';
+  }
+    
+}
