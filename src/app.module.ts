7fb75c80f98bd02092f1be3998dde40f4c7accfc
Henrique Collatto <chcollatto@gmail.com>
2025-04-16 15:47:08 -0300

    alteracao schema prisma e auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,15 @@ import { UsersController } from './controller/user.controller';
 import { UserService } from './service/user.service';
 import { UserRepository } from './repository/user.repository';
 import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
+//import { AuthModule } from './auth/auth.module';
 import { ReportService } from './report/report.service';
 import { ReportController } from './report/report.controller';
 
 @Module({
   controllers: [UsersController, ReportController],
   providers: [UserService, UserRepository, ReportService],
-  imports: [UserModule, AuthModule],
+  imports: [UserModule
+    //  , AuthModule
+  ],
 })
 export class AppModule {}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,30 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from '../prisma.service';
+import { User } from '@prisma/client';
+import * as bcrypt from 'bcrypt'; 
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async validateUser(email: string, pass: string): Promise<User | null> {
+    const user = await this.prisma.user.findUnique({ where: { email } });
+
+    if (user && (await bcrypt.compare(pass, user.password))) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+
+  async login(user: any) {
+    const payload = { email: user.email, sub: user.id };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
+  }
+}
